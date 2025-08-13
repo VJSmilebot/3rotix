5ab48c434d61ec01b8656f41370c48ec989b3072
Smilebot <smilebot3000@gmail.com>
2025-08-13 05:20:17 -0400

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 // pages/_app.js
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>3ROTIX</title>
+        <meta name="description" content="3ROTIX Creator Platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
-  
