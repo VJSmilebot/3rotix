ee87f94e4dea3f2d017f78a65472f1dcf77af6ac
Smilebot <smilebot3000@gmail.com>
2025-08-22 09:19:37 -0400

    placeholder pages

---
+++ pages/learn/onboarding.js
@@ -0,0 +1,7 @@
+export default function Placeholder() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1>🚧 This page is under construction 🚧</h1>
+    </div>
+  );
+}
